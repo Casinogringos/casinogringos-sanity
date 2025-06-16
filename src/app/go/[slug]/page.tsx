31d278c69d81f19eb903e5819698fde721f8ff3b
Jamie Spink <jamiespink2@gmail.com>
2025-06-16 15:50:25 +0200

    small fixes

--- src/app/go/[slug]/page.tsx
+++ src/app/go/[slug]/page.tsx
@@ -1,32 +1,32 @@
-import { notFound } from "next/navigation";
-import { getNodeByUri, getStaticParams } from "@/lib/api";
-import dynamicComponent from "next/dynamic";
-import { AffiliateLink } from "@/types/index";
-const AffiliateItem = dynamicComponent(() => import("@/src/app/AffiliateItem"));
-
-const AffiliatePage = async (props: { params: Promise<{ slug: string }> }) => {
-  const params = await props.params;
-  const affiliate = (await getNodeByUri({
-    uri: `/go/${params?.slug}`,
-  })) as AffiliateLink;
-  if (!affiliate) {
-    return notFound();
-  }
-
-  return <AffiliateItem affiliate={affiliate} />;
-};
-
-export const metadata = {
-  robots: {
-    index: false,
-    nocache: true,
-  },
-};
-
-export async function generateStaticParams() {
-  const affiliates = await getStaticParams("affiliate");
-
-  return affiliates.map(({ node }) => ({ slug: node.slug })) || [];
-}
-
-export default AffiliatePage;
+// import { notFound } from "next/navigation";
+// import { getNodeByUri, getStaticParams } from "@/lib/api";
+// import dynamicComponent from "next/dynamic";
+// import { AffiliateLink } from "@/types/index";
+// const AffiliateItem = dynamicComponent(() => import("@/src/app/AffiliateItem"));
+//
+// const AffiliatePage = async (props: { params: Promise<{ slug: string }> }) => {
+//   const params = await props.params;
+//   const affiliate = (await getNodeByUri({
+//     uri: `/go/${params?.slug}`,
+//   })) as AffiliateLink;
+//   if (!affiliate) {
+//     return notFound();
+//   }
+//
+//   return <AffiliateItem affiliate={affiliate} />;
+// };
+//
+// export const metadata = {
+//   robots: {
+//     index: false,
+//     nocache: true,
+//   },
+// };
+//
+// export async function generateStaticParams() {
+//   const affiliates = await getStaticParams("affiliate");
+//
+//   return affiliates.map(({ node }) => ({ slug: node.slug })) || [];
+// }
+//
+// export default AffiliatePage;
