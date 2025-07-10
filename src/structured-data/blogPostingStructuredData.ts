e9cc821b5e49a2b5dabb2e8450783a1c3749c504
Jamie Spink <jamiespink2@gmail.com>
2025-07-10 21:37:02 +0200

    guide page working

--- src/structured-data/blogPostingStructuredData.ts
+++ src/structured-data/blogPostingStructuredData.ts
@@ -1,37 +1,46 @@
-import { GuidePageSchemaType } from "../schemas/guidePage";
+import { GuidePageSchemaType } from '../schemas/guidePage'
+import { urlFor } from '@/src/lib/client'
 
-export const getBlogPostingStructuredData = ({ page }: { page: GuidePageSchemaType }) => {
-    return {
-        "@context": "https://schema.org",
-        "@type": "BlogPosting",
-        "headline": page.title,
-        "description": page.excerpt,
-        "image": page.featuredImage.image.asset.url,
-        "author": {
-            "@type": "Person",
-            "name": page.author.name,
-            "url": `https://casinogringos.se/experter/${page.author.slug.current}`,
-            "sameAs": [page.author.linkedIn]
-        },
-        "reviewedBy": {
-            "@type": "Person",
-            "name": page.reviewer.name,
-            "url": `https://casinogringos.se/experter/${page.reviewer.slug.current}`,
-            "sameAs": [page.reviewer.linkedIn]
-        },
-        "publisher": {
-            "@type": "Organization",
-            "name": "Casinogringos",
-            "logo": {
-                "@type": "ImageObject",
-                "url": "https://casinogringos.se/images/logo.png"
-            }
-        },
-        "mainEntityOfPage": {
-            "@type": "WebPage",
-            "@id": "https://casinogringos.se/blogg/uttag-leovegas"
-        },
-        "datePublished": page.publishedAt,
-        "dateModified": page.modifiedAt
-    }
+export const getBlogPostingStructuredData = ({
+  page,
+}: {
+  page: GuidePageSchemaType
+}) => {
+  if (!page.author || !page.reviewer) {
+    return null
+  }
+
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'BlogPosting',
+    headline: page.title,
+    description: page.excerpt,
+    image: urlFor(page.seoImage),
+    author: {
+      '@type': 'Person',
+      name: page.author.name,
+      url: `https://casinogringos.se/experter/${page.author.slug.current}`,
+      sameAs: [page.author.linkedIn],
+    },
+    reviewedBy: {
+      '@type': 'Person',
+      name: page.reviewer.name,
+      url: `https://casinogringos.se/experter/${page.reviewer.slug.current}`,
+      sameAs: [page.reviewer.linkedIn],
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: 'Casinogringos',
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://casinogringos.se/images/logo.png',
+      },
+    },
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': 'https://casinogringos.se/blogg/uttag-leovegas',
+    },
+    datePublished: page._createdAt,
+    dateModified: page._updatedAt,
+  }
 }
