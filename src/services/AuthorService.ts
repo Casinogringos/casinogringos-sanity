771d2611a937d28868667fcd78c007a4811fde5e
Jamie Spink <jamiespink2@gmail.com>
2025-10-09 09:56:15 +0200

    small fix

--- src/services/AuthorService.ts
+++ src/services/AuthorService.ts
@@ -5,8 +5,8 @@ class AuthorService {
     validateAuthor(author: AuthorSchemaType) {
         const parse = AuthorSchema.safeParse(author)
         if (!parse.success) {
-            console.log(`Invalid author:\n`, parse.error)
-            fs.writeFileSync('structuredDataError.log', `\n\nInvalid Author\n${JSON.stringify(parse.error)}`)
+            // console.log(`Invalid author:\n`, parse.error)
+            // fs.writeFileSync('structuredDataError.log', `\n\nInvalid Author\n${JSON.stringify(parse.error)}`)
             // return false
         }
         return true
