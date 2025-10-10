d4f744d6309ce39fdc853bdb2085813e71b184f1
Jamie Spink <jamiespink2@gmail.com>
2025-10-10 10:05:00 +0200

    fix: sanity image margin

--- src/components/content/SanityImage.tsx
+++ src/components/content/SanityImage.tsx
@@ -34,18 +34,18 @@ const SanityImage = ({
     lg: 'rounded-lg',
   }
   return (
-    <div className={`${className} ${prose ? '' : 'not-prose'} flex items-center justify-center`}>
+    <div className={`${className} ${prose ? '' : 'not-prose'} flex items-center flex-col justify-center`}>
       <Image
         src={image.src}
         alt={image.alt}
         priority={priority}
         width={width}
         height={height}
-        className={roundedClasses[rounded]}
+        className={`${roundedClasses[rounded]} ${image.caption ? '!mb-0' : ''}`}
         style={{ maxWidth: maxWidth ?? '100%' }}
       />
       {image.caption && (
-        <div className="text-sm text-slate-500 text-center mt-2">
+        <div className="text-sm text-slate-500 text-center">
           <PortableText value={image.caption} />
         </div>
       )}
