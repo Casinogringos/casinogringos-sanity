8e4ee0054717108beeaa845411d4d9ee6c3803c2
Jamie Spink <jamiespink2@gmail.com>
2025-10-22 12:29:24 +0200

    small fix

--- src/components/navigation/BreadCrumbs.tsx
+++ src/components/navigation/BreadCrumbs.tsx
@@ -47,7 +47,7 @@ const BreadCrumbs = ({
                 itemScope
                 itemProp='itemListElement'
                 itemType="https://schema.org/ListItem"
-                itemID={`breadcrumb-${i}`}
+                itemID={item.url}
                 className="flex items-center gap-x-2 lg:gap-x-2"
               >
                 <ChevronRight
@@ -68,6 +68,7 @@ const BreadCrumbs = ({
                   <div
                     itemProp="item"
                     itemType="https://schema.org/WebPage"
+                    itemID={item.text}
                     className="lg:m-l3 hover:text-slate-500 text-xs text-gray-500"
                   >
                     <span itemProp="name">{item.text}</span>
