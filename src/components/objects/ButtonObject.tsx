4758f9ebf5583839d036940528bf712581f00533
Jamie Spink <jamiespink2@gmail.com>
2025-11-18 11:48:18 +0100

    button object variant

--- src/components/objects/ButtonObject.tsx
+++ src/components/objects/ButtonObject.tsx
@@ -1,10 +1,13 @@
 import { ButtonObjectSchemaType } from '@/src/schemas/buttonObject'
 import Link from '@/src/components/content/Link'
 import { formatSlug } from '@/src/lib/utils'
+import { Viaoda_Libre } from 'next/font/google'
 
 const ButtonObject = ({ object }: { object: ButtonObjectSchemaType }) => {
+  let variant
   const getHref = (): string | null => {
     if (object.page?.slug?.current) {
+      variant = 'primary'
       switch (object.page._type) {
         case 'casino-pages':
           return `/${object.page.slug.current}`
@@ -21,9 +24,11 @@ const ButtonObject = ({ object }: { object: ButtonObjectSchemaType }) => {
       }
     }
     if (object.affLink?.slug?.current) {
+      variant = 'affiliate'
       return `/go${formatSlug(object.affLink.slug.current)}`
     }
     if (object.uri) {
+      variant = 'primary'
       return object.uri.current
     }
     return null
@@ -32,7 +37,7 @@ const ButtonObject = ({ object }: { object: ButtonObjectSchemaType }) => {
   const href = getHref()
 
   return href ? (
-    <Link variant="affiliate" href={href}>
+    <Link variant={variant} href={href}>
       {object.title}
     </Link>
   ) : null
