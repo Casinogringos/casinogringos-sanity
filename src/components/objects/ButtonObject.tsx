81cbbef5866b78689b0b9e60dba1a4baf8f281b2
Jamie Spink <jamiespink2@gmail.com>
2025-11-18 11:38:31 +0100

    fix: button object link

--- src/components/objects/ButtonObject.tsx
+++ src/components/objects/ButtonObject.tsx
@@ -3,25 +3,32 @@ import Link from '@/src/components/content/Link'
 import { formatSlug } from '@/src/lib/utils'
 
 const ButtonObject = ({ object }: { object: ButtonObjectSchemaType }) => {
-  const getHref = () => {
-    if (!object.page?.slug?.current) {
-      return `/go${formatSlug(object.affLink?.slug?.current ?? '')}`
+  const getHref = (): string | null => {
+    if (object.page?.slug?.current) {
+      switch (object.page._type) {
+        case 'casino-pages':
+          return `/${object.page.slug.current}`
+        case 'guide-pages':
+          return `/guider/${object.page.slug.current}`
+        case 'news-pages':
+          return `/nyheter/${object.page.slug.current}`
+        case 'slot-pages':
+          return `/slots/${object.page.slug.current}`
+        case 'pages':
+          return `/${object.page.slug.current}`
+        default:
+          return null
+      }
     }
-    switch (object.page._type) {
-      case 'casino-pages':
-        return `/${object.page.slug.current}`
-      case 'guide-pages':
-        return `/guider/${object.page.slug.current}`
-      case 'news-pages':
-        return `/nyheter/${object.page.slug.current}`
-      case 'slot-pages':
-        return `/slots/${object.page.slug.current}`
-      case 'pages':
-        return `/${object.page.slug.current}`
-      default:
-        return null
+    if (object.affLink?.slug?.current) {
+      return `/go${formatSlug(object.affLink.slug.current)}`
     }
+    if (object.uri) {
+      return object.uri.current
+    }
+    return null
   }
+
   const href = getHref()
 
   return href ? (
