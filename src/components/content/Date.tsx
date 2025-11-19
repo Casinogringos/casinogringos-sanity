58e1d74f34b6d3208ef5c4164dd6c67472a36a0c
Jamie Spink <jamiespink2@gmail.com>
2025-11-19 11:10:38 +0100

    removed some console logs and made date component server component

--- src/components/content/Date.tsx
+++ src/components/content/Date.tsx
@@ -1,18 +1,21 @@
-"use client";
-
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 const DynamicDate = ({ timestamp, className }: { timestamp: number, className?: string }) => {
-  const [date, setDate] = useState("");
-  useEffect(() => {
-    const getDate = () =>
-      new Date(timestamp).toLocaleDateString("sv-SE", {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-      })
-    setDate(getDate());
-  }, [timestamp]);
+  // const [date, setDate] = useState("");
+  // useEffect(() => {
+  //   const getDate = () =>
+  //     new Date(timestamp).toLocaleDateString("sv-SE", {
+  //       day: "numeric",
+  //       month: "long",
+  //       year: "numeric",
+  //     })
+  //   setDate(getDate());
+  // }, [timestamp]);
+  const date = new Date(timestamp).toLocaleDateString("sv-SE", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  })
   if (!date) return null;
 
   return <time className={className}>{date}</time>;
