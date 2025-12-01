6562e4d277752ee8b4e5041dcef40a013777cad8
Yannick <yannick@flamingomedia.io>
2025-12-01 13:03:08 +0100

    Fix canonical for authors

--- src/app/om-oss/[slug]/page.tsx
+++ src/app/om-oss/[slug]/page.tsx
@@ -9,13 +9,14 @@ type Params = Promise<{ slug: string }>
 export async function generateMetadata(props: { params: Params }) {
   const params = await props.params
   const author: AuthorSchemaType = await getAuthorBySlug({ slug: params?.slug })
-  const siteURL =
-    (process.env.NEXT_PUBLIC_SITE_URL as string) + author.slug.current
+  const authorURL =
+    (process.env.NEXT_PUBLIC_SITE_URL as string) +
+    `/om-oss/${author.slug.current}`
   const metadata: Metadata = {
     title: author.seoTitle,
     description: author.seoDescription,
     alternates: {
-      canonical: author.canonical ?? new URL(siteURL),
+      canonical: author.canonical ?? authorURL,
     },
     openGraph: {
       title: author.seoTitle,
